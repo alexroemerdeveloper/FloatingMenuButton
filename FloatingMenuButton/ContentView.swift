feb5bec51ed3d6270f902df32b58bf10a041bf94
Alexander Römer <alex@roemer.design>
2019-12-20 16:44:06 +0100

    Initial Commit

---
+++ FloatingMenuButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FloatingMenuButton
+//
+//  Created by Alexander Römer on 20.12.19.
+//  Copyright © 2019 Alexander Römer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
